6639b4c231d740bb82b3d4941c23bc32d93bb280
aaron <schoeffa@gmail.com>
2020-05-25 17:45:59 -0400

    Added 'routing' using ngIf and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  route = 'default';
+
+  handleRouter(routerData: {route: string}) {
+    this.route = routerData.route;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  
+  @Output() router = new EventEmitter<{route: string}>()
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRoute(route: string) {
+    this.router.emit({route: route})
   }
 
 }
